ce2d4c023ebd4029c302eaceb6c2db2c8ec11cab
berkcanemre <berkcan.emre@gmail.com>
2025-06-02 14:23:33 -0400

    completed mysql exercise2

---
+++ BOR-SQL work.sql
@@ -0,0 +1,51 @@
+/*
+1. What categories of products does Northwind Traders sell?
+Beverages, Condiments, Confections, Dairy Products, Grains/Cereals, Meat/Poultry, Produce, Seafood
+
+2. What is the name of a product that sounds interesting to you?
+
+
+3. What are some of the countries in which we have customers?
+Argentina, Austria, Belgium, Brazil, Canada, Denmark, Finland, France, Germany, Ireland, Italy, Mexico, Norway, Poland, Portugal, Spain, Sweden, Switzerland, UK, USA, Venezuela
+
+4. What date was our first order placed? How did you determine that?
+'1996-07-04 00:00:00'
+
+5. What date was our last order placed?
+'1998-05-06 00:00:00'
+
+6. How many customers do we currently have?
+93
+
+7. Do we have any suppliers in South America? Europe? Africa?
+We have suppliers in South America, Europa and Asia but not in Africa.
+
+8. What is our most expensive product?
+'Cte de Blaye' '263.5000'
+
+9. What is our least expensive product?
+'Geitost' '2.5000'
+
+10. We have a customer named "The Big Cheese" if I wanted to get details
+about their last order from us how would you find that information?
+
+a. What tables would you use?
+1-9
+b. How would you link the data in the tables you are using
+c. What information might I want to know about their order?
+i. Order Date?
+ii. Sales Total?
+iii. Product Name?
+iv. Is there other information that you might find interesting?
+
+/*
+'1', 'Beverages', 'Soft drinks, coffees, teas, beers, and ales', ?
+'2', 'Condiments', 'Sweet and savory sauces, relishes, spreads, and seasonings', ?
+'3', 'Confections', 'Desserts, candies, and sweet breads', ?
+'4', 'Dairy Products', 'Cheeses', ?
+'5', 'Grains/Cereals', 'Breads, crackers, pasta, and cereal', ?
+'6', 'Meat/Poultry', 'Prepared meats', ?
+'7', 'Produce', 'Dried fruit and bean curd', ?
+'8', 'Seafood', 'Seaweed and fish', ?
+*/
+
